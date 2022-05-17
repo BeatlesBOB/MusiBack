dcbf9a8d865edead182ba1f887ccd26fe8f429c8
BeatlesBOB <beatlesbob74120@gmail.com>
2022-05-17 21:47:08 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -7,24 +7,26 @@ const router = express.Router();
 
 router.post('/login',(req, res)=> {
   const code = req.body.code;
-  const data = {
-    grant_type:"authorization_code",
-    code:code,
-    redirect_uri:process.env.REDIRECT_URI
-  }
+  const data = new URLSearchParams();
+  data.append('grant_type', 'authorization_code');
+  data.append('code',code);
+  data.append('redirect_uri',process.env.REDIRECT_URI);
+
   const config = {
     headers: {
-      "Authorization": 'Basic ' + (new Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64'))
+      "Authorization": 'Basic ' + (new Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')),
+      "Content-Type":"application/x-www-form-urlencoded"
     },
   }
 
   axios.post('https://accounts.spotify.com/api/token',data,config).then((data)=>{
     res.status(200).json({
-      access_token:data.body.access_token,
-      refresh_token:data.body.refresh_token,
-      expires_in:data.body.expires_in
+      access_token:data.data.access_token,
+      refresh_token:data.data.refresh_token,
+      expires_in:data.data.expires_in
     })
   }).catch(err=>{
+    console.log(err)
     res.status(400).json({msg:err})
   })
 });
